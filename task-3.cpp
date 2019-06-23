cff7bfef7df4b4be3cf4e3458c3a2ebced8310a6
DiankaNanka <diana.dorokhova@bk.ru>
2019-06-24 01:55:36 +0300

    Задание №3

---
+++ task-3.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+ string initialStr;
+ getline(cin, initialStr);
+ string resultString;
+ 
+ for (int i=0; i < initialStr.length(); i++) {
+  if (initialStr[i] == 'A' 
+        || initialStr[i] == 'E'
+        || initialStr[i] == 'I' 
+        || initialStr[i] == 'O'
+        || initialStr[i] == 'U'
+  ) {
+    resultString += tolower(initialStr[i]);
+  } else {
+    resultString += initialStr[i];
+  }
+ }
+
+ cout << resultString;
+
+ return 0;
+}
