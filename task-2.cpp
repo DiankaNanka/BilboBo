b8fe475a5aa5ec593a85546213934c78d822ec4b
DiankaNanka <diana.dorokhova@bk.ru>
2019-06-24 01:51:25 +0300

    Поправила замечание

--- task-2.cpp
+++ task-2.cpp
@@ -5,38 +5,40 @@
 using namespace std;
 
 string countVowels(string bsr) {
- int a=0,
-     e=0,
-     i=0,
-     o=0,
-     u=0,
-     y=0;
- for (int increment=0; increment<bsr.length(); increment++) {
-  if (bsr[increment]=='a') {
-   a++;
+  int a=0,
+      e=0,
+      i=0,
+      o=0,
+      u=0,
+      y=0;
+
+  for (int increment=0; increment<bsr.length(); increment++) {
+    if (bsr[increment]=='a') {
+     a++;
+    }
+    if(bsr[increment]=='e') {
+     e++;
+    }
+    if(bsr[increment]=='i') {
+     i++;
+    }
+    if (bsr[increment]=='o') {
+     o++;
+    }
+    if (bsr[increment]=='u') {
+     u++;
+    }
+    if (bsr[increment]=='y') {
+     y++;
+    }
   }
-  if(bsr[increment]=='e') {
-   e++;
-  }
-  if(bsr[increment]=='i') {
-   i++;
-  }
-  if (bsr[increment]=='o') {
-   o++;
-  }
-  if (bsr[increment]=='u') {
-   u++;
-  }
-  if (bsr[increment]=='y') {
-   y++;
-  }
- }
-return "a = " + std::to_string(a)  + "e =" 
-+ std::to_string(e) + "i =" + 
-std::to_string(i) + "o =" 
-+ std::to_string(o) + "u =" + 
-std::to_string(u) + "y =" + 
-std::to_string(y);
+
+  return "a = " + to_string(a) 
+    + "e =" + to_string(e) 
+    + "i =" + to_string(i) 
+    + "o =" + to_string(o) 
+    + "u =" + to_string(u)
+    + "y =" + to_string(y);
 }
 
 int main() {
@@ -45,4 +47,3 @@ int main() {
  cout << countVowels(bsr);
  return 0;
 }
-
