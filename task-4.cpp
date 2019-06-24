a5d1f47506993ee4c33e5ba33b542b1c1b5cb4b6
DiankaNanka <diana.dorokhova@bk.ru>
2019-06-24 03:36:20 +0300

    Задание №4

---
+++ task-4.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+string changeCase(string consoleInputString) {
+   
+     string result;
+     for (int i=0; i < consoleInputString.length(); i++) 
+     {
+     if (consoleInputString[i] == 'A' 
+        || consoleInputString[i] == 'E'
+        || consoleInputString[i] == 'I' 
+        || consoleInputString[i] == 'O'
+        || consoleInputString[i] == 'U'
+        ) {
+           result += tolower(consoleInputString[i]);
+          } else {
+           result += consoleInputString[i];
+          } 
+     }
+     return result;
+}
+int main() {  
+    string consoleInputString;
+    getline(cin, consoleInputString);
+    cout << changeCase(consoleInputString);
+
+return 0;
+}
